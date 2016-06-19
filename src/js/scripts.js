6b2175388b5d4c38d27fce6aa5c8711e0a4a21d0
root <root@s0565755f.fastvps-server.com>
2016-06-19 15:42:39 -0400

    domains

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,15 +1,2 @@
 //= jquery.min.js
 //= jquery.bxslider.min.js
-$(".button").on('click',function(){
-	show_modal($(this).attr('class').replace('button ',''));
-});
-$(document).on("click",".overlay",function(){
-	if ($(event.target).closest(".modal-inner>div").length) return;
-	$(".overlay").hide();
-});
-function show_modal(form) {
-	$('.modal-inner>div').hide();
-	if (!$('.'+form+'-form').length) return;
-	$('.overlay').css('display','table');
-	$('.'+form+'-form').show();
-}
