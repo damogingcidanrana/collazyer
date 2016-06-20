809e305ee66856957a694f67db81965e8064b0ba
damogingcidanrana <damogingcidanrana@mail.ru>
2016-06-20 05:14:12 +0300

    return scripts

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,19 @@
 //= jquery.min.js
 //= jquery.bxslider.min.js
 //= jquery.imgRotate.js
-
+$(".button").on('click',function(){
+ 	show_modal($(this).attr('class').replace('button ',''));
+ });
+ $(document).on("click",".overlay",function(){
+ 	if ($(event.target).closest(".modal-inner>div").length) return;
+ 	$(".overlay").hide();
+ });
+ function show_modal(form) {
+ 	$('.modal-inner>div').hide();
+ 	if (!$('.'+form+'-form').length) return;
+ 	$('.overlay').css('display','table');
+ 	$('.'+form+'-form').show();
+ } 
 $('.slider-subdomain ul').bxSlider({
   mode: 'fade',
   captions: true
